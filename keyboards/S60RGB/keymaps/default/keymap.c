b7a07d4a0fa8574d26bc79bd40ba9f946774de1d
Martin Brandl <mjisaak@users.noreply.github.com>
2017-01-01 23:42:00 +0100

    Update keymap.c

--- keyboards/S60RGB/keymaps/default/keymap.c
+++ keyboards/S60RGB/keymaps/default/keymap.c
@@ -10,6 +10,7 @@
 // entirely and just use numbers.
 #define _BL 0
 #define _FL 1
+#define _FLTWO 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
@@ -30,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,  KC_RBRC,  KC_BSLS, \
   KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NONUS_HASH, KC_ENT,  \
   KC_LSFT, KC_NONUS_BSLASH,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,            KC_RSFT,  \
-  KC_LCTL, KC_LGUI,KC_LALT,          KC_SPC,                                        KC_RALT,  KC_RGUI, MO(_FL),  MO(_FLTWO)),
+  KC_LCTL, KC_LGUI,KC_LALT,          KC_SPC,                                        KC_RALT,  KC_RGUI, MO(_FL),  MO(_FLTWO),
 
   /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
