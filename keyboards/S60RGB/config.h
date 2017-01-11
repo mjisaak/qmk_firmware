f9b0149c50ce20ec43c0676570896fdacfcd1909
Martin Brandl <mjisaak@users.noreply.github.com>
2017-01-11 10:35:49 +0100

    Update config.h

--- keyboards/S60RGB/config.h
+++ keyboards/S60RGB/config.h
@@ -69,6 +69,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN F6
 #define RGBLIGHT_TIMER
 #define RGBLED_NUM 10    // Number of LEDs
+#define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
